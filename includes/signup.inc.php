e262d5e9a186692433b26fedfb189f8af0b6d69c
ThomasRogersF <Thomasrogersfiguera@gmail.com>
2019-06-29 20:14:10 -0400

    6/29/2019

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -6,14 +6,13 @@ if (isset($_POST['submit'])) {
 
 	$nombre = mysqli_real_escape_string( $conn , $_POST['nombre']);
     $apellido = mysqli_real_escape_string( $conn , $_POST['apellido']);
-	$cargo = mysqli_real_escape_string( $conn , $_POST['cargo']);
-    $Turno = mysqli_real_escape_string( $conn , $_POST['horas']);
 	$cedula = mysqli_real_escape_string( $conn , $_POST['cedula']);
+	$horas = mysqli_real_escape_string( $conn , $_POST['horas']);
 	$guia = mysqli_real_escape_string( $conn , $_POST['guia']);
 	//Error handelers
 	//Check for empty field
 	#second if
-	if (empty($nombre) || empty($apellido) || empty($cargo) || empty($cedula) ||  empty($Turno)) {
+	if (empty($nombre) || empty($apellido) || empty($cedula) ||  empty($horas)) {
 		header("Location: ../signup.php?signup=empty");
    
 	} /*second else*/ else{
@@ -26,19 +25,10 @@ if (isset($_POST['submit'])) {
 		} /*third else*/
 			// Check if password is valid
 			#forth if
-			 /*forth else*/ else {
-				$sql = "SELECT * FROM prof WHERE cedula='$cedula'";
-				$result = mysqli_query($conn , $sql);
-				$resultCheck = mysqli_num_rows($result);
-				#fifth if
-				if ($resultCheck > 0) {
-                    
-					?> <script> alert("usuario tomado"); window.location.href="../signup.php"; </script> <?php exit(); 
-                    
-				} /*forth else*/ else {
+			 /*forth else*/else {
 		
 					//Inser the user into the database
-					$sql = "INSERT INTO prof(nombre , apellido , usuario_cargo , usuario_cedula , usuario_turno , usuario_contra, Pregunta, Respuesta) VALUES ('$nombre','$apellido','$cargo','$cedula','$Turno','$hashedPwd','$pregunta','$hashedQuestion');";
+					$sql = "INSERT INTO prof(nombre, apellido, cedula, horas_laborales, seccion_guia) VALUES ('$nombre','$apellido','$cedula','$horas','$guia');";
 					mysqli_query($conn , $sql);
                     
                    if($sql){ ?> <script> alert("Registro exitoso");</script> <?php exit(); }
@@ -47,7 +37,7 @@ if (isset($_POST['submit'])) {
 				}
 			}
 		}
-	}
+
 
  /*first else*/ else {
 	header("Location: ../signup.php");
--- seleccion_de_materias.php
+++ seleccion_de_materias.php
@@ -8,25 +8,108 @@
 	</head>
     <body>
     <form action="">
-        <label for="Cargo">Materia: </label>
-        
-        <select id="list" name="cargo" required>
-            <option value="">- Selecciona uno -- </option>
-            <option value="CIENCIAS-NATURALES">CIENCIAS NATURALES</option>
-            <option value="BIOLOGIA">BIOLOGIA</option>
-            <option value="QUIMICA">QUIMICA</option>
-            <option value="FISICA">FISICA</option>
-            <option value="EDUCACION-FISICA">EDUCACIÓN FISICA</option>
-            <option value="CASTELLANO">CASTELLANO Y LITERATURA</option>
-            <option value="INGLES">INGLES Y OTRAS LENGUAS EXTRANJERAS 1</option>
+        <label for="Cargo">Profesor: </label>
+        <select name="" id="">
+        <option value="">- Selecciona uno -- </option>
+
         </select><br><br>
 
-        <label for="Año">Años: </label>      
-        <select id="grado">
-         <option value="">-- Selecciona uno -- </option>
-        </select>
+        <label for="Cargo">Materia: </label>
+        <select id="list" name="cargo" required>
+            <option value="">- Selecciona uno -- </option>
+            <option value="CIENCIAS-NATURALES-1">CIENCIAS NATURALES 1</option>
+            <option value="CIENCIAS-NATURALES-2">CIENCIAS NATURALES 2</option>
+            <option value="BIOLOGIA-1">BIOLOGIA 1</option>
+            <option value="BIOLOGIA-2">BIOLOGIA 2</option>
+            <option value="BIOLOGIA-3">BIOLOGIA 3</option>
+            <option value="QUIMICA-1">QUIMICA 1</option>
+            <option value="QUIMICA-2">QUIMICA 2</option>
+            <option value="QUIMICA-3">QUIMICA 3</option>
+            <option value="FISICA">FISICA-1</option>
+            <option value="FISICA">FISICA-2</option>
+            <option value="FISICA">FISICA-3</option>
+            <option value="EDUCACION-FISICA-1">EDUCACIÓN FISICA 1</option>
+            <option value="EDUCACION-FISICA-2">EDUCACIÓN FISICA 2</option>
+            <option value="EDUCACION-FISICA-3">EDUCACIÓN FISICA 3</option>
+            <option value="EDUCACION-FISICA-4">EDUCACIÓN FISICA 4</option>
+            <option value="EDUCACION-FISICA-5">EDUCACIÓN FISICA 5</option>
+            <option value="CASTELLANO-1">CASTELLANO Y LITERATURA</option>
+            <option value="CASTELLANO-2">CASTELLANO Y LITERATURA</option>
+            <option value="CASTELLANO-3">CASTELLANO Y LITERATURA</option>
+            <option value="CASTELLANO-4">CASTELLANO Y LITERATURA</option>
+            <option value="CASTELLANO-5">CASTELLANO Y LITERATURA</option>
+            <option value="INGLES-1">INGLES Y OTRAS LENGUAS EXTRANJERAS 1</option>
+            <option value="INGLES-2">INGLES Y OTRAS LENGUAS EXTRANJERAS 2</option>
+            <option value="INGLES-3">INGLES Y OTRAS LENGUAS EXTRANJERAS 3</option>
+            <option value="INGLES-4">INGLES Y OTRAS LENGUAS EXTRANJERAS 4</option>
+            <option value="INGLES-5">INGLES Y OTRAS LENGUAS EXTRANJERAS 5</option>
+         
+        </select><br><br>
+        
+
+        <table class="table table-bordered" id="dynamic_field">
+        <tr>
+        <td><select id="grado" name="name[]" id="name" ><option value="">-- Selecciona uno -- </option>
+               <option value="">- Selecciona uno -- </option>
+            <option value="CIENCIAS-NATURALES-1">CIENCIAS NATURALES 1</option>
+            <option value="CIENCIAS-NATURALES-2">CIENCIAS NATURALES 2</option>
+            <option value="BIOLOGIA-1">BIOLOGIA 1</option>
+            <option value="BIOLOGIA-2">BIOLOGIA 2</option>
+            <option value="BIOLOGIA-3">BIOLOGIA 3</option>
+            <option value="QUIMICA-1">QUIMICA 1</option>
+            <option value="QUIMICA-2">QUIMICA 2</option>
+            <option value="QUIMICA-3">QUIMICA 3</option>
+            <option value="FISICA">FISICA-1</option>
+            <option value="FISICA">FISICA-2</option>
+            <option value="FISICA">FISICA-3</option>
+            <option value="EDUCACION-FISICA-1">EDUCACIÓN FISICA 1</option>
+            <option value="EDUCACION-FISICA-2">EDUCACIÓN FISICA 2</option>
+            <option value="EDUCACION-FISICA-3">EDUCACIÓN FISICA 3</option>
+            <option value="EDUCACION-FISICA-4">EDUCACIÓN FISICA 4</option>
+            <option value="EDUCACION-FISICA-5">EDUCACIÓN FISICA 5</option>
+            <option value="CASTELLANO-1">CASTELLANO Y LITERATURA</option>
+            <option value="CASTELLANO-2">CASTELLANO Y LITERATURA</option>
+            <option value="CASTELLANO-3">CASTELLANO Y LITERATURA</option>
+            <option value="CASTELLANO-4">CASTELLANO Y LITERATURA</option>
+            <option value="CASTELLANO-5">CASTELLANO Y LITERATURA</option>
+            <option value="INGLES-1">INGLES Y OTRAS LENGUAS EXTRANJERAS 1</option>
+            <option value="INGLES-2">INGLES Y OTRAS LENGUAS EXTRANJERAS 2</option>
+            <option value="INGLES-3">INGLES Y OTRAS LENGUAS EXTRANJERAS 3</option>
+            <option value="INGLES-4">INGLES Y OTRAS LENGUAS EXTRANJERAS 4</option>
+            <option value="INGLES-5">INGLES Y OTRAS LENGUAS EXTRANJERAS 5</option>
+            </select></td>
+        <td><button name="add" id="add">Agregar</button></td>
+        </tr>
+        </table>
+        
+
 
     </form>
+    <script>  
+ $(document).ready(function(){  
+      var i=1;  
+      $('#add').click(function(){  
+           i++;  
+           $('#dynamic_field').append('<tr id="row'+i+'"><td><select id="grado" name="name[]" ><option value="">-- Selecciona uno -- </option><option value="">- Selecciona uno -- </option><option value="CIENCIAS-NATURALES-1">CIENCIAS NATURALES 1</option><option value="CIENCIAS-NATURALES-2">CIENCIAS NATURALES 2</option><option value="BIOLOGIA-1">BIOLOGIA 1</option><option value="BIOLOGIA-2">BIOLOGIA 2</option><option value="BIOLOGIA-3">BIOLOGIA 3</option><option value="QUIMICA-1">QUIMICA 1</option><option value="QUIMICA-2">QUIMICA 2</option><option value="QUIMICA-3">QUIMICA 3</option><option value="FISICA">FISICA-1</option><option value="FISICA">FISICA-2</option><option value="FISICA">FISICA-3</option><option value="EDUCACION-FISICA-1">EDUCACIÓN FISICA 1</option><option value="EDUCACION-FISICA-2">EDUCACIÓN FISICA 2</option><option value="EDUCACION-FISICA-3">EDUCACIÓN FISICA 3</option><option value="EDUCACION-FISICA-4">EDUCACIÓN FISICA 4</option><option value="EDUCACION-FISICA-5">EDUCACIÓN FISICA 5</option><option value="CASTELLANO-1">CASTELLANO Y LITERATURA</option><option value="CASTELLANO-2">CASTELLANO Y LITERATURA</option><option value="CASTELLANO-3">CASTELLANO Y LITERATURA</option><option value="CASTELLANO-4">CASTELLANO Y LITERATURA</option><option value="CASTELLANO-5">CASTELLANO Y LITERATURA</option><option value="INGLES-1">INGLES Y OTRAS LENGUAS EXTRANJERAS 1</option><option value="INGLES-2">INGLES Y OTRAS LENGUAS EXTRANJERAS 2</option><option value="INGLES-3">INGLES Y OTRAS LENGUAS EXTRANJERAS 3</option><option value="INGLES-4">INGLES Y OTRAS LENGUAS EXTRANJERAS 4</option><option value="INGLES-5">INGLES Y OTRAS LENGUAS EXTRANJERAS 5</option></select></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');  
+      });  
+      $(document).on('click', '.btn_remove', function(){  
+           var button_id = $(this).attr("id");   
+           $('#row'+button_id+'').remove();  
+      });  
+      $('#submit').click(function(){            
+           $.ajax({  
+                url:"name.php",  
+                method:"POST",  
+                data:$('#add_name').serialize(),  
+                success:function(data)  
+                {  
+                     alert(data);  
+                     $('#add_name')[0].reset();  
+                }  
+           });  
+      });  
+ });  
+ </script>
 
     <script src='./js-functions/cambio-de-opciones.js'></script>
 
