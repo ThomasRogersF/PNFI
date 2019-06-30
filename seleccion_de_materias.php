44cd07f5c8b01e66259bf78e35df982ffb68b7d2
ThomasRogersF <Thomasrogersfiguera@gmail.com>
2019-06-29 20:15:01 -0400

    Update seleccion_de_materias.php

--- seleccion_de_materias.php
+++ seleccion_de_materias.php
@@ -15,36 +15,6 @@
         </select><br><br>
 
         <label for="Cargo">Materia: </label>
-        <select id="list" name="cargo" required>
-            <option value="">- Selecciona uno -- </option>
-            <option value="CIENCIAS-NATURALES-1">CIENCIAS NATURALES 1</option>
-            <option value="CIENCIAS-NATURALES-2">CIENCIAS NATURALES 2</option>
-            <option value="BIOLOGIA-1">BIOLOGIA 1</option>
-            <option value="BIOLOGIA-2">BIOLOGIA 2</option>
-            <option value="BIOLOGIA-3">BIOLOGIA 3</option>
-            <option value="QUIMICA-1">QUIMICA 1</option>
-            <option value="QUIMICA-2">QUIMICA 2</option>
-            <option value="QUIMICA-3">QUIMICA 3</option>
-            <option value="FISICA">FISICA-1</option>
-            <option value="FISICA">FISICA-2</option>
-            <option value="FISICA">FISICA-3</option>
-            <option value="EDUCACION-FISICA-1">EDUCACIÓN FISICA 1</option>
-            <option value="EDUCACION-FISICA-2">EDUCACIÓN FISICA 2</option>
-            <option value="EDUCACION-FISICA-3">EDUCACIÓN FISICA 3</option>
-            <option value="EDUCACION-FISICA-4">EDUCACIÓN FISICA 4</option>
-            <option value="EDUCACION-FISICA-5">EDUCACIÓN FISICA 5</option>
-            <option value="CASTELLANO-1">CASTELLANO Y LITERATURA</option>
-            <option value="CASTELLANO-2">CASTELLANO Y LITERATURA</option>
-            <option value="CASTELLANO-3">CASTELLANO Y LITERATURA</option>
-            <option value="CASTELLANO-4">CASTELLANO Y LITERATURA</option>
-            <option value="CASTELLANO-5">CASTELLANO Y LITERATURA</option>
-            <option value="INGLES-1">INGLES Y OTRAS LENGUAS EXTRANJERAS 1</option>
-            <option value="INGLES-2">INGLES Y OTRAS LENGUAS EXTRANJERAS 2</option>
-            <option value="INGLES-3">INGLES Y OTRAS LENGUAS EXTRANJERAS 3</option>
-            <option value="INGLES-4">INGLES Y OTRAS LENGUAS EXTRANJERAS 4</option>
-            <option value="INGLES-5">INGLES Y OTRAS LENGUAS EXTRANJERAS 5</option>
-         
-        </select><br><br>
         
 
         <table class="table table-bordered" id="dynamic_field">
